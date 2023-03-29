5bd3be165e1c7a29ab93cf8684f764026ec78903
Lykaxx <xlykaios@gmail.com>
2023-03-29 21:42:09 +0200

    V1.2
    + added enemy sprites
    + added enemy animations and collisions
    + added and fixed Attack Systems
    + added link to levelOne

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
-    public swordAttack Attack;
+    public SwordAttack Attacco;
+
     Vector2 movementInput;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
     bool canMove = true;
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,73 +27,88 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void FixedUpdate()
-    {   
-        if(canMove){
-            if(movementInput != Vector2.zero)
-            {
-            bool success = TryMove(movementInput);
-            if(!success && movementInput.x > 0){
-                success = TryMove(new Vector2(movementInput.x, 0));
+    private void FixedUpdate() {
+        if(canMove) {
+            // If movement input is not 0, try to move
+            if(movementInput != Vector2.zero){
+                
+                bool success = TryMove(movementInput);
 
-                    if(!success && movementInput.y > 0){
-                    success = TryMove(new Vector2(movementInput.y, 0));
-                    }
-            }
+                if(!success) {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
 
-            animator.SetBool("isMoving", success);
+                if(!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+                
+                animator.SetBool("isMoving", success);
             } else {
                 animator.SetBool("isMoving", false);
             }
-            if(movementInput.x < 0){
+
+            // Set direction of sprite to movement direction
+            if(movementInput.x < 0) {
                 spriteRenderer.flipX = true;
-             
-             }
-            if(movementInput.x > 0){
+            } else if (movementInput.x > 0) {
                 spriteRenderer.flipX = false;
-            
-            }    
             }
+        }
     }
 
-    private bool TryMove(Vector2 direction){
-     if(direction != Vector2.zero){
-                
-         int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+    private bool TryMove(Vector2 direction) {
+        if(direction != Vector2.zero) {
+            // Check for potential collisions
+            int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
             if(count == 0){
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime ); 
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
                 return true;
-            }else{ return false; }
-            }else{ return false; }
-}
+            } else {
+                return false;
+            }
+        } else {
+            // Can't move if there's no direction to move in
+            return false;
+        }
+        
+    }
 
-    void OnMove(InputValue movementValue)
-    {
+    void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
     }
 
-    void OnFire(){
-        animator.SetTrigger("SpecAttack");
+    void OnFire() {
+        animator.SetTrigger("swordAttack");
+        print("Fire Pressed");
     }
 
-    public void Swing(){
-        lockMovement();
+    public void SwordAttack() {
+        LockMovement();
+
         if(spriteRenderer.flipX == true){
-            Attack.AttackLeft();
-        }else{
-            Attack.AttackRight();
+            Attacco.AttackLeft();
+            print("Fire Left");
+        } else {
+            Attacco.AttackRight();
+            print("Fire Right");
         }
     }
-    
-    public void lockMovement(){
+
+    public void EndSwordAttack() {
+        UnlockMovement();
+        Attacco.StopAttack();
+    }
+
+    public void LockMovement() {
         canMove = false;
     }
-    public void unlockMovement(){
+
+    public void UnlockMovement() {
         canMove = true;
     }
 }
---
+++ Assets/Scripts/SwordAttack.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Collider2D swordCollider;
+    public float damage = 3;
+    Vector2 rightAttackOffset;
+
+    private void Start() {
+        rightAttackOffset = transform.position;
+    }
+
+    public void AttackRight() {
+        swordCollider.enabled = true;
+        print("Attacked Right");
+        transform.localPosition = rightAttackOffset;
+    }
+
+    public void AttackLeft() {
+        swordCollider.enabled = true;
+        print("Attacked left");
+        transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+    }
+
+    public void StopAttack() {
+        swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        print("ATK LOADING ###");
+        if(other.tag == "Enemy") {
+            print("Loaded ATK #######");
+            // Deal damage to the enemy
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if(enemy != null) {
+                enemy.Health -= damage;
+                print("Attack Done");
+            }
+        }
+    }
+}
